14598774db86431b3018dd56368a5bae443a03aa
karthikakella <venkatajairam@gmail.com>
2017-04-16 02:21:28 -0400

    Command line program

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -1,7 +1,2 @@
 #!/bin/bash 
-JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
-CLASSPATH="/home/akella/Desktop/Assignment" . 
-$inputDir1='/home/akella/Desktop/Assignment/iliad'
-$inputDir2='/home/akella/Desktop/Assignment/odyssey'
-java  -Djava.library.path=./lib/path/Lib -classpath .:lib/path/Lib/mylib.jar Mainclass "$questionNum" "$inputDir1" "$inputDir2" 
-exit 0
+java MainSparkClass "$@"
